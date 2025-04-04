60b5820501b7d899ba3c3fc16018fb8428b601fb
gurup <guruprasadsatsangi15@gmail.com>
2025-04-04 17:37:27 +0530

    update code

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const app = express();
 let port =3000;//8080
 
 app.listen(port,()=>{
-    console.log(`app listening on port ${port}`);
+    console.log(`app listening on ports ${port}`);
 })
 
-app.use((req,res)=>{
+/*app.use((req,res)=>{
     console.log("request recived");
     //console.log(req);
     //res.send("this is string");
@@ -18,4 +18,20 @@ app.use((req,res)=>{
         color:"red",
     });
     
-});
+});*/
+app.get("/", (req, res) => {
+    res.send("You connected to the root path");
+  });
+  
+  app.get("/search", (req, res) => {
+    res.send("You connected to the search path");
+  });
+  
+  app.get("/home", (req, res) => {
+    res.send("You connected to the home page");
+  });
+  
+
+  /*app.get("*", (req, res) => {
+    res.send("You're connected to a non-existent path");
+  });*/
